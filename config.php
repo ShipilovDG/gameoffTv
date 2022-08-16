64ce767d952c24013945377d3a4ecc88ea6669e8
Daniil Shipilov <d.shipilov@bronevik.com>
2022-08-16 19:09:18 +0500

    fix gitignore

---
+++ config.php
@@ -0,0 +1,25 @@
+<?php
+// +------------------------------------------------------------------------+
+// | @author Deen Doughouz (DoughouzForest)
+// | @author_url 1: http://www.playtubescript.com
+// | @author_url 2: http://codecanyon.net/user/doughouzforest
+// | @author_email: wowondersocial@gmail.com
+// +------------------------------------------------------------------------+
+// | PlayTube - The Ultimate Video Sharing Platform
+// | Copyright (c) 2017 PlayTube. All rights reserved.
+// +------------------------------------------------------------------------+
+// MySQL Hostname
+$sql_db_host = "localhost:3306";
+// MySQL Database User
+$sql_db_user = "root";
+// MySQL Database Password
+$sql_db_pass = "1";
+// MySQL Database Name
+$sql_db_name = "";
+
+// Site URL
+$site_url = "http://localhost:8091"; // e.g (http://example.com)
+
+// Purchase code
+$purchase_code = "12312312-1234-1234-2134-234123412"; // Your purchase code, don't give it to anyone.
+?>
